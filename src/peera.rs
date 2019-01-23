62668a6364cad4b032b5b2604d81a45da8f92cbc
Naja Melan <najamelan@autistici.org>
2019-01-23 21:28:52 +0100

    2 peers, no multithreading

--- src/peera.rs
+++ src/peera.rs
@@ -6,18 +6,18 @@ fn main() -> Result< (), NngFail >
 	println!( "PeerA: starting." );
 
 	const URLB: &str = "ipc:///tmp/peerAB";
-	const URLC: &str = "ipc:///tmp/peerAC";
+	// const URLC: &str = "ipc:///tmp/peerAC";
 
 	let factory = Latest::default();
 
-	let pairB   = factory.pair_open()?.listen( &URLB )?;
-	let pairC   = factory.pair_open()?.listen( &URLC )?;
+	let pair_b   = factory.pair_open()?.listen( &URLB )?;
+	// let pair_c   = factory.pair_open()?.listen( &URLC )?;
 
-	let mut receivedB = pairB.recv()?;
-	let mut receivedC = pairC.recv()?;
+	let mut received_b = pair_b.recv()?;
+	// let mut received_c = pair_c.recv()?;
 
-	println!( "PeerA: Received message: {:#?}", std::str::from_utf8( receivedB.body() ) );
-	println!( "PeerA: Received message: {:#?}", std::str::from_utf8( receivedC.body() ) );
+	println!( "PeerA: Received message: {:#?}", std::str::from_utf8( received_b.body() ) );
+	// println!( "PeerA: Received message: {:#?}", std::str::from_utf8( received_c.body() ) );
 
 	println!( "PeerA: stopping." );
 
